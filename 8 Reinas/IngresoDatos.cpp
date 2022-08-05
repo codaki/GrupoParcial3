98de9dcce62364731b55abae599ebb077120445c
DeniseRea <105187806+DeniseRea@users.noreply.github.com>
2022-08-05 07:02:08 -0500

    Update IngresoDatos.cpp

--- 8 Reinas/IngresoDatos.cpp
+++ 8 Reinas/IngresoDatos.cpp
@@ -1,3 +1,19 @@
+/*
+	*Universidad de las Fuerzas Armadas "ESPE"
+   
+     *Autores:
+        De Veintemilla Luca
+	Iza Christopher 
+	Rea Denise
+	Vargas Kevin
+
+	*Fecha de creacion: 
+        * 04-08-2022
+	*Fecha de modificacion:
+        * 05-08-2022
+    *GitHub del grupo:	
+        https://github.com/ChristopherIza/GrupoParcial3.git
+*/
 #include "IngresoDatos.hpp"
 #include <iostream>
 #include <stdlib.h>
