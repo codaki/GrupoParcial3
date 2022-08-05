de2e2dab3a852879dc645c4efb764a4c768f6d39
DeniseRea <105187806+DeniseRea@users.noreply.github.com>
2022-08-05 07:03:23 -0500

    Update Reina.hpp

--- 8 Reinas/Reina.hpp
+++ 8 Reinas/Reina.hpp
@@ -1,3 +1,19 @@
+/*
+	*Universidad de las Fuerzas Armadas "ESPE"
+   
+     *Autores:
+        De Veintemilla Luca
+	Iza Christopher
+	Rea Denise
+	Vargas Kevin
+
+	*Fecha de creacion: 
+        * 04-08-2022
+	*Fecha de modificacion:
+        * 05-08-2022
+    *GitHub del grupo:	
+        https://github.com/ChristopherIza/GrupoParcial3.git
+*/
 #include <iostream>
 #include <fstream>
 #define DIMENSION 8
