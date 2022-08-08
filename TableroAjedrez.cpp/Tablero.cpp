cbd9d797e01e340bb9ba06af72d7e23543be6763
ChristopherIza <cdiza5@espe.edu.ec>
2022-08-08 10:11:38 -0500

    Encabezado

--- TableroAjedrez.cpp/Tablero.cpp
+++ TableroAjedrez.cpp/Tablero.cpp
@@ -1,3 +1,18 @@
+/*
+	*Universidad de las Fuerzas Armadas "ESPE"
+   
+     *Autores:
+    De Veintemilla Luca
+	Iza Christopher 
+	Rea Denise
+	Vargas Kevin
+	*Fecha de creacion: 
+        * 06-08-2022
+	*Fecha de modificacion:
+        * 07-08-2022
+    *GitHub del grupo:	
+        https://github.com/ChristopherIza/GrupoParcial3.git
+*/
 #include <iostream>
 #define MARCA 219
 int main(int argc, char **argv){
