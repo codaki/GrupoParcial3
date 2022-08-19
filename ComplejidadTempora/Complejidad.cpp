b851949e59734a2b43c1f0ace5cd2baf9373e03d
ChristopherIza <cdiza5@espe.edu.ec>
2022-08-19 09:45:31 -0500

    Encabezados

--- ComplejidadTempora/Complejidad.cpp
+++ ComplejidadTempora/Complejidad.cpp
@@ -1,3 +1,18 @@
+/*
+	*Universidad de las Fuerzas Armadas "ESPE"
+   
+     *Autores:
+        De Veintemilla Luca
+	Iza Christopher
+	Rea Denise
+	Vargas Kevin
+	*Fecha de creacion: 
+        * 18-08-2022
+	*Fecha de modificacion:
+        * 19-08-2022
+    *GitHub del grupo:	
+        https://github.com/ChristopherIza/GrupoParcial3.git
+*/
 #include <iostream>
 #include <chrono>
 #include "Complejidad.hpp"
